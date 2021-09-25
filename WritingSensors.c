0300b41e349f6158f74f82c49db9851d5459bdfb
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2021-09-25 16:31:42 +0200

    Update

--- WritingSensors.c
+++ WritingSensors.c
@@ -10,22 +10,27 @@ bool sensors [235];
 
 int main() {
     srand(time(NULL));
-
     CreateSensors();
-    SwitchRandomSensors();
-
+    while (true) {
+        delay(1);
+        SwitchRandomSensors();
+    }
     return 0;
 }
 
-
-
 SwitchRandomSensors() { // Switches 20 random sensors true>false / false>true
-    fptr = fopen("C:\\SensorFiles\\SensorFile.txt","w");
-
     for(i = 0; i < 21; i++) {
         int x = (rand() % (upper - lower + 1)) + lower;
+        if (sensors[x] == true){
+            sensors[x] = false;
+        } else {
+            sensors[x] = true;
+        }
     }
-
+    fptr = fopen("C:\\SensorFiles\\SensorFile.txt","w");
+    for(i = 0; i < 236; i++) {
+        fwrite(sensors[i] ? "1" : "0", sizeof(char), 1, fptr);
+    }
     fclose(fptr);
 }
 
@@ -36,4 +41,10 @@ CreateSensors() { // Creates 236 sensors and writes them to a file with random o
         fwrite(sensors[i] ? "1" : "0", sizeof(char), 1, fptr);
     }
     fclose(fptr);
+}
+
+delay(int number_of_seconds) { // Creates a time delay 
+    int milli_seconds = 1000 * number_of_seconds;
+    clock_t start_time = clock();
+    while (clock() < start_time + milli_seconds);
 }
