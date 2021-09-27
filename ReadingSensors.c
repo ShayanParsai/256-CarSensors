56dd348a5e3599c613023b1bec7fde69c24ea4b1
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2021-09-27 13:45:51 +0200

    Update

--- ReadingSensors.c
+++ ReadingSensors.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 FILE *fptr;
 int i = 0;
-bool sensors [235];
+bool currentSensors [235];
+bool newSensors [235];
 
 int main() {
+    for (i = 0; i < 236; i++) {
+        currentSensors[i] = false;
+    }
+    for (i = 0; i < 236; i++) {
+        newSensors[i] = false;
+    }
+
     
-    fptr = fopen("C:\\SensorFiles\\SensorFile.txt","r");
-
-
-    fclose(fptr);
+    while (true) {
+        delay(2);
+        fptr = fopen("C:\\SensorFiles\\SensorFile.txt","r");
+        // newSensors = take values from file
+        CheckActiveSensors();
+        currentSensors = newSensors;
+        fclose(fptr);
+    }
+    
     return 0;
 }
 
 CheckActiveSensors() { // Check and alarm active sensors
-    for (i = 0; i <= 236; i++) {
-        if (sensors[i]) {
-            printf("\nSensor number %d is active", i);
+    for (i = 0; i < 236; i++) {
+        if (currentSensors[i] != newSensors[i]) {
+            printf("\nSensor number %d has switched state", i);
         }
     }
+}
+
+delay(int number_of_seconds) { // Creates a time delay 
+    int milli_seconds = 1000 * number_of_seconds;
+    clock_t start_time = clock();
+    while (clock() < start_time + milli_seconds);
 }
