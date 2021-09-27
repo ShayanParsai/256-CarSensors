7c72aced4d2f54db89382a76e44ea39c2694164d
Shayan Parsai <70860248+ShayanParsai@users.noreply.github.com>
2021-09-27 18:36:15 +0200

    Update

--- ReadingSensors.c
+++ ReadingSensors.c
@@ -1,43 +1,48 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 FILE *fptr;
 int i = 0;
 int x = 0;
-unsigned char currentSensors [30];
-unsigned char newSensors [30];
+int c;
+unsigned int currentSensors [15];
+unsigned int newSensors [15];
+struct {
+   unsigned int testSensors : 16;
+} testSensors;
 
 int main() {
+    testSensors.testSensors = 65535;
     resetArrays();
 
-    while (true) {
-        delay(2);
-        fptr = fopen("C:\\SensorFiles\\SensorFile.txt","r");
-        ////////////////////// Todo : Read from file, put content in newSensors as unsigned char
-        
-        for (x = 0; x < 30; x++) {
-            int bit_pos_value = 128;
-            int bit_value = 0;
-            for (i = 0; i < 8; i++) {
-                if( 'thefile[0-7]' == '1'){
-                    
-                }
-            }
-        newSensors[x] = bit_value;
+    fptr = fopen("C:\\SensorFiles\\SensorFile.txt","r");
+    
+    char tempChunk [16] = "";
+
+    while ((c = getc(fptr)) != EOF) {
+        char tmp = (char)c;
+        size_t len = strlen(tempChunk);
+        snprintf(tempChunk + len, sizeof tempChunk - len, "%c", tmp);
+        printf("\n%s",tempChunk);
+        x++;
+        if (x == 16) {
+            testSensors.testSensors = atoi(tempChunk);
         }
     }
-        ////////////////////// Todo ^
-        CheckActiveSensors();
-        currentSensors = newSensors;
-        fclose(fptr);
-    }
+      
+    
+    CheckActiveSensors();
+
+    fclose(fptr);
+    overwriteArrays();
     return 0;
 }
 
 CheckActiveSensors() { // Check and alarm switched sensors
-    for (i = 0; i < 30; i++) {
+    for (i = 0; i < 15; i++) {
         if (currentSensors[i] != newSensors[i]) {
             printf("\nSensor number %d has switched state", i); // todo : dosent work right now
         }
@@ -51,8 +56,15 @@ delay(int number_of_seconds) { // Creates a time delay
 }
 
 resetArrays() {
-    for (i = 0; i < 30; i++) {
+    for (i = 0; i < 15; i++) {
         currentSensors[i] = 0;
         newSensors[i] = 0;
     }
 }
+
+overwriteArrays() {
+    for (i = 0; i < 15; i++) {
+        currentSensors[i] = newSensors[i];
+    }
+}
+
